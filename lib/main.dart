f77970d789333c2022978a5e3fb0f1b7f03a74e3
AEManov20 <AEManov20@codingburgas.bg>
2023-05-10 16:25:26 +0300

    Bug fix

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class _HomePageState extends State<HomePage> {
           UserPost(
               firstName: "Alexander",
               lastName: "Manov",
-              bigTitle: "shite",
+              bigTitle: "title",
               postContent:
                   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
               datePosted: DateTime.now(),
