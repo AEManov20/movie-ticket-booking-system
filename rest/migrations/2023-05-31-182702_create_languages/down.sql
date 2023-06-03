3d26073017b242862378611b166154806774bd64
Alexander Manov <AEManov20@codingburgas.bg>
2023-06-03 18:41:42 +0300

    Fix schema reversion problems

--- rest/migrations/2023-05-31-182702_create_languages/down.sql
+++ rest/migrations/2023-05-31-182702_create_languages/down.sql
@@ -1,3 +1,5 @@
 -- This file should undo anything in `up.sql`
 
+ALTER TABLE IF EXISTS theatre_screenings DROP CONSTRAINT IF EXISTS subtitles_language_fkey;
+ALTER TABLE IF EXISTS theatre_screenings DROP CONSTRAINT IF EXISTS audio_language_fkey;
 DROP TABLE IF EXISTS languages;
