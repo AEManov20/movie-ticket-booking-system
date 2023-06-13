ba22fe667321b6a12bce07ba8d9e97ad765d1454
Alexander Manov <AEManov20@codingburgas.bg>
2023-06-13 16:40:22 +0300

    Update halls migration to comply with schema.rs

--- rest/migrations/2023-05-19-194013_create_halls/up.sql
+++ rest/migrations/2023-05-19-194013_create_halls/up.sql
@@ -3,7 +3,6 @@
 CREATE TABLE IF NOT EXISTS halls (
     "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     theatre_id UUID NOT NULL REFERENCES theatres("id"),
-    "name" VARCHAR(250) NOT NULL,
-    price_increase FLOAT NOT NULL DEFAULT 0,
+    "name" VARCHAR(50) NOT NULL,
     seat_data JSON NOT NULL
 );
