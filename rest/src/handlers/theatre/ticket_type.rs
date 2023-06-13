af84c13030d8af35de4f905940032106d8c956c1
Alexander Manov <AEManov20@codingburgas.bg>
2023-06-13 17:08:46 +0300

    Implement create/delete_ticket_type

--- rest/src/handlers/theatre/ticket_type.rs
+++ rest/src/handlers/theatre/ticket_type.rs
@@ -24,11 +24,51 @@ async fn create_ticket_type(
     bridge_role_service: web::Data<BridgeRoleService>,
     claims: JwtClaims
 ) -> Result<TicketType> {
-    let Some(theatre_res) = theatre_service.get_by_id(path.into_inner()).await? else {
+    let theatre_id = path.into_inner();
+    let (_, user) = user_res_from_jwt(&claims, &user_service).await?;
+    let Some(theatre_res) = theatre_service.get_by_id(theatre_id).await? else {
         return Err(ErrorType::NotFound)
     };
 
+    if !user.is_super_user {
+        check_roles!(
+            [Role::TheatreOwner, Role::TicketManager],
+            user.id,
+            theatre_id,
+            bridge_role_service,
+            role_service
+        );
+    }
+
     Ok(theatre_res.create_ticket_type(new_ticket_type.into_inner()).await?.into())
+}
+
+#[delete("/{ttid}")]
+async fn delete_ticket_type(
+    path: web::Path<(uuid::Uuid, uuid::Uuid)>,
+    theatre_service: web::Data<TheatreService>,
+    user_service: web::Data<UserService>,
+    role_service: web::Data<RoleService>,
+    bridge_role_service: web::Data<BridgeRoleService>,
+    claims: JwtClaims
+) -> Result<()> {
+    let (theatre_id, ticket_type_id) = path.into_inner();
+    let (_, user) = user_res_from_jwt(&claims, &user_service).await?;
+    let Some(theatre_res) = theatre_service.get_by_id(theatre_id).await? else {
+        return Err(ErrorType::NotFound)
+    };
+
+    if !user.is_super_user {
+        check_roles!(
+            [Role::TheatreOwner, Role::TicketManager],
+            user.id,
+            theatre_id,
+            bridge_role_service,
+            role_service
+        );
+    }
+
+    Ok(theatre_res.delete_ticket_type(ticket_type_id).await?.into())
 }
 
 pub fn config(cfg: &mut web::ServiceConfig) {
