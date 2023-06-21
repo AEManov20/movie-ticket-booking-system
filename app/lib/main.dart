0f83e0505bb7e3e883ce0b8ab27715fb4879e048
Alexander Manov <AEManov20@codingburgas.bg>
2023-06-21 20:42:05 +0300

    Add simple navbar

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -31,12 +31,53 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: PostView(),
-      bottomNavigationBar: GNav(tabs: [
-        GButton(icon: Icons.favorite),
-        GButton(icon: Icons.account_box),
-      ]),
+    return Scaffold(
+      body: const PostView(),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 20,
+              color: Colors.black.withOpacity(.1),
+            )
+          ],
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+            child: GNav(
+                rippleColor: Colors.grey[300]!,
+                hoverColor: Colors.grey[100]!,
+                gap: 8,
+                activeColor: Colors.black,
+                iconSize: 24,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                duration: const Duration(milliseconds: 200),
+                tabBackgroundColor: Colors.grey[100]!,
+                color: Colors.black,
+                tabs: const [
+                  GButton(
+                    icon: Icons.home,
+                    text: 'Home',
+                  ),
+                  GButton(
+                    icon: Icons.monitor_heart,
+                    text: 'Likes',
+                  ),
+                  GButton(
+                    icon: Icons.search,
+                    text: 'Search',
+                  ),
+                  GButton(
+                    icon: Icons.account_box,
+                    text: 'Profile',
+                  )
+                ]),
+          ),
+        ),
+      ),
     );
   }
 }
